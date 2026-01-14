56d84d26b99faf9bed62066ec096d00caecea9b4
Siam <siamthelegendman@gmail.com>
2026-01-14 15:00:55 +0600

    Enhanced Home page layout and style the background

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      This is the home page
+    <main className='flex justify-center items-center min-h-screen bg-sky-200'>
+      <div className='w-full max-w-screen-xl mx-auto p-10 bg-slate-100 rounded-2xl shadow-lg'>
+        <h1 className='text-green-500 font-bold text-5xl'>HOME PAGE</h1>
     </div>
+    </main>
+    
   )
 }
 
